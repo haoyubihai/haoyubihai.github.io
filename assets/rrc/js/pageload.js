8518d5a41b159eb87e00068b53d681fb6b7bf661
浩宇碧海 <jrhhybh@163.com>
2016-06-13 17:03:42 +0800

    goodsdetail

--- assets/rrc/js/pageload.js
+++ assets/rrc/js/pageload.js
@@ -6,3 +6,30 @@ $(function(){
 $("#city-picker").cityPicker({
 	title: "请选择项目所在地"
 });
+//按城市筛选
+
+$("#address_sort").cityPicker({
+	title: "城市选择"
+});
+//筛选
+$("#sort").select({
+  title: "排序",
+  items: [
+    {
+      title: "默认排序",
+      value: "001",
+    },
+    {
+      title: "价格最低",
+      value: "002",
+    },
+    {
+      title: "价格最高",
+      value: "003",
+    },
+    {
+      title: "评价最多",
+      value: "004",
+    },
+  ]
+});
