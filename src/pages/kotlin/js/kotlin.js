9d7b9a460dc0b9708ca21c1c1bd0f2b93e519fdf
jiarh <jrhhybh@163.com>
2019-08-23 18:04:59 +0800

    index.html

--- src/pages/kotlin/js/kotlin.js
+++ src/pages/kotlin/js/kotlin.js
@@ -22,9 +22,7 @@ $.getJSON("../kotlin/asset/kotlin-content.json","",function (data) {
 
         $(".ul_content").delegate("#id-author","click",function () {
             var $index = $(this).parents('#li_item').index();
-            // window.location.href = data.data[$index].author_url;
-            alert(data.data[$index].author_url);
-            window.location.href = "http://www.baidu.com";
+            window.location.href = data.data[$index].author_url;
             return false
         })
     })
