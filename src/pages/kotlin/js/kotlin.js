21fce3a49449e84e2cd521c405de918408fdedc0
jiarh <jrhhybh@163.com>
2019-08-23 15:14:16 +0800

    logo and  color

--- src/pages/kotlin/js/kotlin.js
+++ src/pages/kotlin/js/kotlin.js
@@ -1,4 +1,4 @@
-$.getJSON("../asset/kotlin-content.json","",function (data) {
+$.getJSON("../../kotlin/asset/kotlin-content.json","",function (data) {
     $.each(data,function (item) {
         console.log(item)
     })
