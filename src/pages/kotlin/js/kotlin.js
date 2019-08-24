64089fb189d70afcc88e9b5e1fdb0d1da47862a4
jiarh <jrhhybh@163.com>
2019-08-24 14:32:42 +0800

    cs02

--- src/pages/kotlin/js/kotlin.js
+++ src/pages/kotlin/js/kotlin.js
@@ -3,17 +3,17 @@ $.getJSON("../kotlin/asset/kotlin-content.json","",function (data) {
         $.each(item,function (index,content) {
             console.log(index+"***"+content.title)
             var $item = $("<li id='li_item'>\n" +
-                "                <div class=\"item\">\n" +
+                "                <div class='item'>\n" +
                 "                    <a href="+content.url+">\n" +
-                "                        <div class=\"item_l\">\n" +
+                "                        <div class='item_l'>\n" +
                 "                            <h2>"+content.title+"</h2>\n" +
-                "                            <div class=\"a-summary sub-title\">"+content.summary+"</div>\n" +
-                "                            <div class=\"title-b\">\n" +
-                "                                <div class=\"i-author\"><span id='id-author'>"+content.author+"</span></div>\n" +
-                "                                <div class=\"i-time\">"+content.time+"</div>\n" +
+                "                            <div class='a-summary sub-title'>"+content.summary+"</div>\n" +
+                "                            <div class='title-b'>\n" +
+                "                                <div class='i-author'><span id='id-author'>"+content.author+"</span></div>\n" +
+                "                                <div class='i-time'>"+content.time+"</div>\n" +
                 "                            </div>\n" +
                 "                        </div>\n" +
-                "                        <div class=\"item_r\"><img src="+content.carver+" alt=\"\"></div>\n" +
+                "                        <div class='item_r'><img src="+content.carver+" alt='></div>\n" +
                 "                    </a>\n" +
                 "                </div>\n" +
                 "            </li>");
